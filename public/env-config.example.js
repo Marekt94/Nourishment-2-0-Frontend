4080cde3015ea9134d09e64f8c4fac9766dd1948
Marekt94 <marekt94@gmail.com>
2026-03-11 12:58:06 +0100

    backend url is injected dynamically

---
+++ public/env-config.example.js
@@ -0,0 +1,4 @@
+window._env_ = {
+  // Replace this with your actual API endpoint URL when deploying
+  API_BASE_URL: "http://example.com/api",
+};
---
+++ public/env-config.js
@@ -0,0 +1,3 @@
+window._env_ = {
+  API_BASE_URL: "http://192.168.0.42:8080",
+};
